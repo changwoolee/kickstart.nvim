d081737d70ecae620d35a5acb0f09a04a5139d63
Changwoo Lee <changle@gu-changle-lv.qualcomm.com>
2026-06-18 11:09:22 -0700

    devcompute

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -4,7 +4,7 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
 	build = ":TSUpdate",
-    main = 'nvim-treesitter.configs',
+--     main = 'nvim-treesitter.configs',
 	cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
 	lazy = vim.fn.argc(-1) == 0, -- load treesitter early when opening a file from the cmdline
 	opts = {
